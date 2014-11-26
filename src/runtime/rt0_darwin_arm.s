219be263626ef292635fd9dded412c40ef043c08
minux <minux.ma@gmail.com>
2014-11-26 05:47:20 -0500

    runtime/rt0_darwin_arm.s: restore the ability to do internal linking.

--- src/runtime/rt0_darwin_arm.s
+++ src/runtime/rt0_darwin_arm.s
@@ -3,14 +3,8 @@
 // license that can be found in the LICENSE file.
 
 #include "textflag.h"
-//#define TEST_EXT_LINK
 
 TEXT _rt0_arm_darwin(SB),7,$-4
-#ifdef TEST_EXT_LINK
-	// make sure when testing extlink, internal linked program will crash
-	MOVW	$0, R0
-	MOVW	R0, 0(R0)
-#endif
 	// prepare arguments for main (_rt0_go)
 	MOVW	(R13), R0	// argc
 	MOVW	$4(R13), R1		// argv
@@ -21,11 +15,7 @@ TEXT main(SB),NOSPLIT,$-8
 	// disable runfast mode of vfp
 	EOR		R12, R12
 	WORD	$0xeee1ca10 // fmxr	fpscr, ip
-#ifdef TEST_EXT_LINK
-	// prepare arguments for main (_rt0_go)
-	MOVW	(R13), R0	// argc
-	MOVW	$4(R13), R1		// argv
-#endif
+	// save argc and argv onto stack
 	MOVM.DB.W [R0-R1], (R13)
 	MOVW	$runtime·rt0_go(SB), R4
 	B		(R4)

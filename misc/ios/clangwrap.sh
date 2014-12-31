e727b88bd95b85835651158d837cc240f77e8c1b
minux <minux.ma@gmail.com>
2014-12-31 01:14:51 -0500

    misc/ios/clangwrap.sh: update script based on @josharian's recommendation
    https://go-review.googlesource.com/#/c/2127/5/misc/ios/clangwrap.sh

--- misc/ios/clangwrap.sh
+++ misc/ios/clangwrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# NOTE: please update deployment target and -isysroot to match your setup.
+# This uses the latest available iOS SDK, which is recommended.
+# To select a specific SDK, run 'xcodebuild -showsdks'
+# to see the available SDKs and replace iphoneos with one of them.
+SDK=iphoneos
+SDK_PATH=`xcrun --sdk $SDK --show-sdk-path`
 export IPHONEOS_DEPLOYMENT_TARGET=5.1
 # cmd/cgo doesn't support llvm-gcc-4.2, so we have to use clang.
-exec clang -arch armv7 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk "$@"
+CLANG=`xcrun --sdk $SDK --find clang`
+exec $CLANG -arch armv7 -isysroot $SDK_PATH "$@"

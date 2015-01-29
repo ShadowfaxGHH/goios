59e1363be741a5e61eacf2b759a457ac1e76169e
minux <minux.ma@gmail.com>
2015-01-29 02:36:03 -0500

    cmd/ld: fix a typo.
    https://go-review.googlesource.com/#/c/2118/6

--- src/cmd/ld/lib.c
+++ src/cmd/ld/lib.c
@@ -267,9 +267,9 @@ loadlib(void)
 	tlsg = linklookup(ctxt, "runtime.tlsg", 0);
 	// For most ports, runtime.tlsg is a placeholder symbol for TLS
 	// relocation. However, the Android and Darwin ports need it to
-	// be a real variable. Instead of hard-code which platforms need
-	// it to be a real variable, we set the type to STLSBSS only when
-	// the runtime has not declared the type of it already.
+	// be a real variable. Instead of hard-coding which platforms
+	// need it to be a real variable, we set the type to STLSBSS only
+	// when the runtime has not declared its type already.
 	if(tlsg->type == 0)
 		tlsg->type = STLSBSS;
 	tlsg->size = PtrSize;

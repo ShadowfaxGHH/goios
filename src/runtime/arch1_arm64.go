2465bbdf9f64b28322587c89e8f58bb3bd1497e5
Minux <minux@golang.org>
2015-03-04 23:49:50 -0500

    runtime/arch1_arm64.go: turns out darwin/arm64 is using 16KB pages

--- src/runtime/arch1_arm64.go
+++ src/runtime/arch1_arm64.go
@@ -9,7 +9,7 @@ const (
 	_BigEndian        = 0
 	_CacheLineSize    = 32
 	_RuntimeGogoBytes = 64
-	_PhysPageSize     = 65536*goos_nacl + 4096*(1-goos_nacl)
+	_PhysPageSize     = 65536*goos_nacl + 4096*(1-goos_nacl)*(1-goos_darwin) + 16384*goos_darwin
 	_PCQuantum        = 4
 	_Int64Align       = 8
 )
